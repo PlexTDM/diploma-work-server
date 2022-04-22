791ed07a53a79d4438bf955f7c1ed124beb1c6e9
PlexTDM <tengisteka0631@gmail.com>
2022-04-23 00:29:31 +0800

    html

--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,6 @@ const ArticleController = require('../controllers/article');
 const AuthController = require('../controllers/auth');
 const path = require('path');
 
-router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 // router.get('/', ArticleController.getArticles);
 router.get('/latest/:num', ArticleController.getLatest);
 router.get('/idsearch/:id', ArticleController.getArticlesById);
@@ -22,5 +19,9 @@ router.post('/register', AuthController.registerUser);
 router.post('/login', AuthController.loginUser);
 router.put('/updateUser/:id', AuthController.updateUser);
 router.post('/accesstoken', AuthController.getAccessToken);
+
+router.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = router;
